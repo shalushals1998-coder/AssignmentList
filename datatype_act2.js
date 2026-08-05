41097c4251a40778a0f1279e4c9939d627817c9d
shalushals1998-coder <shalushals1998@gmail.com>
2026-08-05 22:38:37 +0530

    Add files via upload
    Day2Activity

---
+++ datatype_act2.js
@@ -0,0 +1,17 @@
+const browserVersion = 'Chrome';
+//const browserVersion2 = 'Firefox';
+
+function getBrowserVersion(){
+
+    if(browserVersion == 'Chrome'){
+       var browserVersion = 'firefox';
+       //let browserVersion = 'firefox';
+       //const browserVersion
+
+       console.log('inside block',browserVersion);
+
+    }
+
+    console.log('accessing variable inside the block', browserVersion);
+}
+getBrowserVersion()
