d6e30e4cf77befbad94915f00f93c007b22d1f2f
shalushals1998-coder <shalushals1998@gmail.com>
2026-08-11 08:47:55 +0530

    Add files via upload

---
+++ oddNum.js
@@ -0,0 +1,11 @@
+function printOddNumbers(){
+
+    for(let i= 1;i<=25;i+=2){
+        if(i % 2 != 0){
+            console.log("The odd numbers between 1 to 25",i)
+
+        }
+
+        
+    }
+}printOddNumbers()
